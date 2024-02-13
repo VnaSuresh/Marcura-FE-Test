682970fb75971f58f405b73916c45cc7f07667ee
VnaSuresh <34338064+VnaSuresh@users.noreply.github.com>
2024-02-13 18:47:31 +0400

    Adding logic to fit map based on bounds

--- src/app/route-visualizer/route-map/route-map.component.ts
+++ src/app/route-visualizer/route-map/route-map.component.ts
@@ -1,8 +1,7 @@
 import { Component, ViewChild, ChangeDetectorRef } from '@angular/core';
-import * as XLSX from 'xlsx';
 import { RoutesService } from '../services/routes.service';
 import { Papa } from 'ngx-papaparse';
-import { GoogleMap, MapPolyline } from '@angular/google-maps';
+import { GoogleMap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-route-map',
@@ -10,6 +9,7 @@ import { GoogleMap, MapPolyline } from '@angular/google-maps';
   styleUrl: './route-map.component.css',
 })
 export class RouteMapComponent {
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
   options: google.maps.MapOptions = {
     center: { lat: 40, lng: -20 },
     zoom: 6
@@ -21,6 +21,7 @@ export class RouteMapComponent {
   polylineOptions: any = {
     strokeColor: 'red', strokeWeight: 5, path: []
   }
+  
 
   constructor(private routesService: RoutesService, private papa: Papa) { }
 
@@ -48,14 +49,28 @@ export class RouteMapComponent {
   drawRoute(route: any) {
     const points = JSON.parse(route.points);
     this.polylineOptions.path = [];
+    this.markerPositions = [];
     points.forEach((point: any, index: number) => {
       this.polylineOptions.path.push({ lat: point[0], lng: point[1] });
       if (index === 0) {
         this.options.center = { lat: point[0], lng: point[1] };
         this.markerPositions = [{ lat: point[0], lng: point[1] }];
         this.options = { ...this.options };
+      } else if (index === points.length - 1) {
+        this.markerPositions.push({ lat: point[0], lng: point[1] })
       }
     })
     this.polylineOptions = { ...this.polylineOptions };
+    this.adjustMapBounds();
+  }
+
+  adjustMapBounds() {
+    const bounds = new google.maps.LatLngBounds();
+    for (const point of this.polylineOptions?.path) {
+      bounds.extend(new google.maps.LatLng(point.lat, point.lng));
+    }
+    setTimeout(() => {
+      this.map.fitBounds(bounds);
+    }, 0);
   }
 }
